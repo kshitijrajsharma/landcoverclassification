b1709716d39e37376fbb9c4a51f26e395197652a
kshitijrajsharma <skshitizraj@gmail.com>
2025-10-06 16:36:26 +0200

    fix : fixes bug on the inconsistency of input images

--- tif2cog.sh
+++ tif2cog.sh
@@ -4,21 +4,22 @@
 [ ! -d "$1" ] && { echo "Error: Directory not found"; exit 1; }
 
 INPUT_FOLDER="$1"
+COG_FOLDER="${INPUT_FOLDER}/cog"
 
-mapfile -d '' files < <(find "$INPUT_FOLDER" -type f \( -iname "*.tif" -o -iname "*.tiff" \) -print0)
+mkdir -p "$COG_FOLDER"
+
+mapfile -d '' files < <(find "$INPUT_FOLDER" -maxdepth 1 -type f \( -iname "*.tif" -o -iname "*.tiff" \) -print0)
+total=${#files[@]}
+count=0
 
 for tif_file in "${files[@]}"; do
     filename=$(basename "$tif_file")
-    
-    if gdalinfo "$tif_file" 2>/dev/null | grep -q "LAYOUT=COG"; then
-        continue
-    fi
-
-    echo " Converting: $filename"
-    dir=$(dirname "$tif_file")
     base_name="${filename%.*}"
     extension="${filename##*.}"
-    output_file="${dir}/${base_name}_cog.${extension}"
+    output_file="${COG_FOLDER}/${base_name}.${extension}"
     
-    gdal_translate -q -of COG -co COMPRESS=LZW -co BIGTIFF=YES "$tif_file" "$output_file"
+    ((count++))
+    echo "$count/$total Converting: $filename"
+    
+    gdal_translate -q -of COG -co COMPRESS=LZW -co BIGTIFF=YES -ot Byte -scale "$tif_file" "$output_file"
 done
